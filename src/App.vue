409806f4e1649c854c67452c042090aac052326f
aluno <aluno@gmail.com>
2019-04-24 10:10:22 -0300

    Teste

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <template>
+<div>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Arca Animail</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Adoção</v-btn>
+            <v-btn flat>Apadrinhamento</v-btn>
+            <v-btn flat>Doação</v-btn>
+            <v-btn flat>contatos</v-btn>
+            <v-btn flat>ONG Arca Animal</v-btn>
+            <v-text-field
+                placehoolder="pesquisa"></v-text-field>
+
+            ></v-toolbar-items>
+        
+    </v-toolbar>
     <v-content>
-      
     </v-content>
+    </div>
+
 </template>
+<script>
+    export default {
+        data() {
+            return{
+                links:[
+                    {
+                        text: "Cachorros"
+                    },
+                    {
+                        text: "Gatos"
+                    },
+                    {
+                        text: "Serpentes"
+                    }
+                ],
+                drawer: false
+            }
+        },
+        methods:{
+            toggleDrawer() {
+                this.drawer = this.drawer
+            }
+        }
+    }
+</script>
