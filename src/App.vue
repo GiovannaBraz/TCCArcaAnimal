c412f815db372628ddf92ac4fb4f7d74b37f727c
aluno <aluno@gmail.com>
2019-05-16 11:13:11 -0300

    raffa

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,39 @@
                 placehoolder="pesquisa"
                 box
                 append-icon="search"
+                class= "black--text"
+                @click="exibe = lexibe"
             ></v-text-field>
-            
         </v-toolbar-items>   
     </v-toolbar>
+        <v-navigation-drawer absolute permanent>
+            <v-toolbar color="teal lighten-2 white--text">
+            <h1>Barbearia</h1>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list dense>
+            <v-list-tile to="/">
+                <v-list-tile-action>
+                <v-icon color="teal">dashboard</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                Início
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile to="/login">
+                <v-list-tile-action>
+                <v-icon color="teal">person</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                Login
+                </v-list-tile-content>
+            </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
     </v-content>
 </v-app>
-
 </template>
 <script>
     export default {
