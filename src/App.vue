6c82dbebb4c8c333569cc359f927f180ed0fe418
aluno <aluno@gmail.com>
2019-05-17 11:22:49 -0300

    raffa

--- src/App.vue
+++ src/App.vue
@@ -25,16 +25,49 @@
          temporary
          v-model="exibe"
     >
-    
+    <v-toolbar color="teal lighten-2 white--text">
+            <h1>Arca Animal</h1>
+          </v-toolbar>
+          <v-divider></v-divider>
+          <v-list dense>
+            <v-list-tile to="/">
+              <v-list-tile-action>
+                <v-icon color="teal">dashboard</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                Início
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile to="/login">
+              <v-list-tile-action>
+                <v-icon color="teal">person</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                Login
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile to="/cartao">
+              <v-list-tile-action>
+                <v-icon color="teal">credit_card</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                Exemplo de Cartão
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
         <v-content>
-        <router-view></router-view>
+            <router-view></router-view>
         </v-content>
     </v-app>
+
     </template>
     <script>
         export default {
             data() {
                 return{
+                    exibe: false,
+                    logado: false
                 }
             },
             methods:{
