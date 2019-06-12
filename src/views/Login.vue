61148e568628a3266755ef5fb4df6e2be4417a03
aluno <aluno@gmail.com>
2019-06-12 08:53:35 -0300

    raffa

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,15 @@
 <template>
-		<v-layout>
+		<v-layout align-center justify-center fill-height>
 			<v-flex xs12 sm6>
-				sadfsadfdsafsda
+				<v-img
+					src="https://i.etsystatic.com/7725562/r/il/04fda1/1753814517/il_794xN.1753814517_rzrt.jpg"
+				></v-img>	
+				<v-img
+					src="https://www.stickdecor.com.br/wp-content/uploads/2017/05/papel-de-parede-adesivo-tijolo-old-school-tj-004-2-600x600.jpg"
+				></v-img>
 			</v-flex>
 			<v-flex xs12 sm6>
-				<v-card class="pa-2">
+				<v-card>
 					<v-text-field label="E-mail" outline/>
 					<v-text-field label="Senha" outline/>
 					<h1>Teste</h1>
