c4410bf6fab3367fc2eabbb090a5d1360a9392e7
aluno <aluno@gmail.com>
2019-07-31 09:31:38 -0300

    raffa

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
 	<v-layout justify-center align-center fill-height>
 		<v-flex xs6 >
-			<v-card class="pa-4">
+			<v-card class="pa-3">
 				<v-text-field  label="E-mail" class="pb-3"/>
 				<v-text-field  label="Senha" />
-				<v-btn color="info">Login</v-btn>
+				<v-btn color="cyan lighten-1">Login</v-btn>
 			</v-card>
 		</v-flex>
 	</v-layout>
